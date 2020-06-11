c11cd4c0296e3f699eb73d388a0f5032b7e2e869
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-06-11 14:03:54 -0500

    Set up basic pages.

---
+++ src/pages/services.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+export default class services extends Component {
+  render() {
+    return (
+      <div>
+        <h1>this is our services page</h1>
+      </div>
+    )
+  }
+}
